f41914fab1786ae7e842e705aa813a0fd354711e
chenrd <chenruida@outlook.com>
2022-04-20 22:09:15 +0800

    init

---
+++ config/config.default.ts
@@ -0,0 +1,25 @@
+import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
+import { mysql } from './config.mysql';
+
+export default (appInfo: EggAppInfo) => {
+  const config = {} as PowerPartial<EggAppConfig>;
+
+  // override config from framework / plugin
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_';
+
+  // add your egg config in here
+  config.middleware = [];
+
+  config.mysql = mysql;
+  // add your special config in here
+  const bizConfig = {
+    sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
+  };
+
+  // the return config will combines to EggAppConfig
+  return {
+    ...config,
+    ...bizConfig,
+  };
+};
